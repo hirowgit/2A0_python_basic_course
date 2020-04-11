b5259e2e14ac29b341e0480dd1cfb959ba2797ca
Hiro <hiro@HiroWaga-Retina-MacBook-Pro-2.local>
2020-04-11 23:34:47 +0900

    Create lec1_step2.py

---
+++ lec1_step2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+fruits = ['strawberry', 'grape', 'tomato'];
+print(fruits);
+
+
+# In[3]:
+
+
+fruits = ['strawberry', 'grape', 'tomato'];
+for x in fruits:
+  print(x)
+
