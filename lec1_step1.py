0903359ddf148b6fdf75da281adaa25e7473966b
Hiro <hiro@HiroWaga-Retina-MacBook-Pro-2.local>
2020-04-11 23:35:20 +0900

    Update lec1_step1.py

--- lec1_step1.py
+++ lec1_step1.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[2]:
+
+
+fruits = ['strawberry', 'grape', 'tomato'];
+print(fruits);
+
+
 # In[3]:
 
 
-fruits = ["apple", "banana", "cherry"]
+fruits = ['strawberry', 'grape', 'tomato'];
 for x in fruits:
   print(x)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
